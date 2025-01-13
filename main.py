0176fb6efaf84711b8481bbaad695122a9ba5345
braden <leachbr787@northwested.org>
2025-01-13 13:42:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+#Braden Leach
+#January 13 2024
+#Local Scope Challenge
 
+
+
+
+#----1----#
+
+# def greet(fname):
+#     print(f'Hello, {fname} your name is a local scope!')
+# greet('braden')
+
+#----2----#
+def count_vowels(word):
+    vowels = 'aeiou'
+    count = 0
+    for char in word:
+        if char in vowels:
+            count += 1
+    return word
+count_vowels(word = 'parameter')
+
+
+
+
+#----3----#
